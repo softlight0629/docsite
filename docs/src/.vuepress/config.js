0155d7130f7c942852cd964ecc92f986db8be2e8
云泽 <xuebin.gxb@leoao-inc.com>
2023-08-04 16:37:45 +0800

    init

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,84 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: '摩客文档',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  base: '/docs/',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '指南',
+        link: '/guide/',
+      },
+      {
+        text: '快速入门',
+        link: '/quick/'
+      },
+      {
+        text: '使用教程',
+        link: '/quick/'
+      },
+      {
+        text: '开发文档',
+        link: '/devdoc/'
+      },
+      {
+        text: '博客',
+        link: '/guide/',
+      },
+      {
+        text: '教学案例',
+        link: '/case/'
+      },
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
